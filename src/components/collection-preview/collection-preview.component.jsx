6defcdb5820b92d37b1dba88eb2c527cef434c7d
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-06 16:08:47 +0000

    70 Collection Item Changes

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,16 @@
 /* Collection-Preview Components*/
 import React from 'react';
-import './collection-preview.styles.sass';
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-        {items
+      {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-         <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>

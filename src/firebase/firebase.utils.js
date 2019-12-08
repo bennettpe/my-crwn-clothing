2c99231620e0000e133e832cda554504cf4876e4
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-08 13:28:12 +0000

    81. Google Sign in Authentication Changes

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCJj-VkBjkA9iG53JrRgxVoWsaFBj3BkDc",
-  authDomain: "my-crwn-db-7a068.firebaseapp.com",
-  databaseURL: "https://my-crwn-db-7a068.firebaseio.com",
-  projectId: "my-crwn-db-7a068",
-  storageBucket: "my-crwn-db-7a068.appspot.com",
-  messagingSenderId: "325464700114",
-  appId: "1:325464700114:web:15ee08e6444012e52ce90b",
-  measurementId: "G-RHJ6N8RWFY"
+  
 };
 
 firebase.initializeApp(config);

6aecb38b426f06ac7607ad4865bdc83eca18da89
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-08 19:06:17 +0000

    81. Google Sign in Authentication Changes (Fix error)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up-component";
 import Header from './components/header/header.component';
-//import { auth } from './firebase/firebase.utils';
 
 function App() {
   return (
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,12 +1,12 @@
-/* Firebase Utilities */
+// Firebase Utilities 
 import firebase from 'firebase/app';
-import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/firestore';
 
-/* Get Firebase Config */
+// Get Firebase Config 
 const config = require('../config')
 
-var firebaseConfig = {
+let firebaseConfig = {
   apiKey: config.firebase.apiKey,
   authDomain: config.firebase.authDomain,
   databaseURL: config.firebase.databaseURL,
@@ -15,13 +15,16 @@ var firebaseConfig = {
   messageingSenderId: config.firebase.messageingSenderId,
   appId: config.firebase.appId,
   measurementId: config.firebase.measurementId
-};
+}; 
 
+// Initialize Firebase 
 firebase.initializeApp(firebaseConfig);
 
+// make auth and firestore references 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// Goole Auth Provider
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);

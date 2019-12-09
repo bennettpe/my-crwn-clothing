1ef28a5fcd7f34d04ec405df2d6e1ca4041ce88e
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-09 20:31:02 +0000

    85. Google Sign In Authentication 3 Changes

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.setState({ email: '', password:'' });
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = (event) => {
@@ -27,32 +27,34 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name='email'
-            type='email'
+            name="email"
+            type="email"
             handleChange={this.handleChange}
             value={this.state.email}
-            label='email'
+            label="email"
             required
           />
           <FormInput
-            name='password'
-            type='password'
+            name="password"
+            type="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label='password'
+            label="password"
             required
           />
-          <CustomButton type='submit'> Sign In </CustomButton> 
-          <CustomButton onClick={signInWithGoogle}> 
-          {' '}
-          Sign in with Google{' '}
-          </CustomButton> 
+          <div className="buttons">
+            <CustomButton type="submit"> Sign In </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {' '}
+              Sign in with Google{' '}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );

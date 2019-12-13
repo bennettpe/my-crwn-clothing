54c1eb67c62db9517209fba7ef4b25d245c8b0c3
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-13 20:05:29 +0000

    104 Cart Component Changes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
-import { CartIcon } from '../cart-icon/cart-icon.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 

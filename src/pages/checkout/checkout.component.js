4975cc445816da29268e464fee2ef66c39cb3337
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-15 20:19:24 +0000

    117. Checkout Page Changes

---
+++ src/pages/checkout/checkout.component.js
@@ -0,0 +1,10 @@
+// Checkout Component
+import React from 'react';
+import './checkout.styles.scss';
+
+const CheckoutPage = () => 
+    <div>
+      CHECKOUT PAGE
+    </div>;
+
+export default CheckoutPage;

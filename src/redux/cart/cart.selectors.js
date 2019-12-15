ea5b8fdc2f76459bcae694ecfbe317d9b14c3aae
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-15 11:04:39 +0000

    Reselect Library Changes

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+// Cart Selectors
+import { createSelector } from 'reselect';
+
+// Input Selector
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);

c53651a566b78e3c391d952807e9c616c7811803
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-18 10:39:05 +0000

    127. Collection State Into Redux Changes

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,9 @@
+// Shop Selectors
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+);

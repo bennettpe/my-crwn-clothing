ef58dec72c08f4fa1c516fc45a930be41285001e
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-19 15:57:14 +0000

    135. Data Flow In Our App Changes

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,13 +4,17 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  shop => shop.collections
 );
 
-export const selectCollection = collectionUrlParm => 
-    createSelector(
-        [selectCollections],
-        collections => 
-          collections[collectionUrlParm]
-    );
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );

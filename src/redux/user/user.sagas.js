9505ced79ca9cdebabc0d4ea5a28dce0b55a0459
bennettpe <37184112+bennettpe@users.noreply.github.com>
2019-12-31 14:53:03 +0000

    186.Sign Out With Sagas Changes

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -3,7 +3,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { SignInSuccess, SignInFailure, } from './user.actions';
+import { SignInSuccess, SignInFailure, signOutSuccess, signOutFailure } from './user.actions';
 
 import { auth, googleProvider, createUserProfileDocument, getCurrentUser } from '../../firebase/firebase.utils';
 
@@ -45,6 +45,15 @@ export function* isUserAuthenticated() {
     }
 }
 
+export function* signOut() {
+    try {
+      yield auth.signOut();
+      yield put(signOutSuccess());
+    } catch (error) {
+      yield put(signOutFailure(error));
+    }
+}
+
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -57,6 +66,15 @@ export function* onCheckUserSession() {
     yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
-export function* userSagas() {
-    yield all([call(onGoogleSignInStart), call(onEmailSignInStart), call(isUserAuthenticated)]);
+export function* onSignOutStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 }
+
+export function* userSagas() {
+    yield all([
+      call(onGoogleSignInStart),
+      call(onEmailSignInStart),
+      call(isUserAuthenticated),
+      call(onSignOutStart)
+    ]);
+  }

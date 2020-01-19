5ae16963115fbe210db1fe8d32c87b15246a0486
bennettpe <37184112+bennettpe@users.noreply.github.com>
2020-01-19 11:28:17 +0000

    240.Exersise Mobile Friendly App Changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 import { store, persistor} from './redux/store';
 
